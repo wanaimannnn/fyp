505dc2bd5bd5e89f75dc0f3f3291c66a7e1084bc
Unknown <wanaimannn@gmail.com>
2017-01-07 23:20:03 +0800

    tukar js

--- calendar.php
+++ calendar.php
@@ -237,7 +237,7 @@
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
