fbddfea9ae567bb66e13f36a5757db8cc703daf0
Unknown <wanaimannn@gmail.com>
2017-01-08 01:49:14 +0800

    link admin

--- calendaradmin.php
+++ calendaradmin.php
@@ -47,7 +47,7 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="index.html">Admin Page</a>
+              <a class="navbar-brand" href="adminhome.html">Admin Page</a>
           </div>
           <!-- Top Menu Items -->
           <ul class="nav navbar-right top-nav">
