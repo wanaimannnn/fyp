4cc37190dcf83f93a313b2007662e8c533721b5f
Unknown <wanaimannn@gmail.com>
2017-01-07 23:56:24 +0800

    try db

--- dbConfig.php
+++ dbConfig.php
@@ -1,12 +1,12 @@
 <?php
 //db details
-$dbHost = 'localhost';
+$dbServer= 'localhost';
 $dbUsername = 'root';
 $dbPassword = 'opom2317';
 $dbName = 'permohonan';
 
 //Connect and select the database
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+$db = new mysqli($dbServer, $dbUsername, $dbPassword, $dbName);
 
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
