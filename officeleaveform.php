0096bde6efab0a48c77f51bee9731189e1924984
Wan Aiman <wanaimannn@gmail.com>
2017-01-09 01:28:25 +0800

    connect office leave form dgn database

---
+++ officeleaveform.php
@@ -0,0 +1,28 @@
+<?php
+
+$db = "permohonan";
+$link = mysqli_connect("localhost","root","opom2317");
+
+if (! $link) die (mysqli_error());
+mysqli_select_db($link,'permohonan') or die("Select Error:" . mysqli_error());
+
+
+$name = $_POST['name'];
+$department = $_POST['department'];
+foreach($_POST['chkbox'] as $index => $val);
+$date = date('Y-m-d',strtotime($_POST['date']));
+$radio= $_POST['purpose'];
+$reason=mysqli_real_escape_string($link, $_POST['reason']);
+
+// $end_date= date('Y-m-d',strtotime($_POST['end_date']));
+// $verification = $_POST['verification'];
+
+$result = mysqli_query ($link,"INSERT INTO request (name,department,shift,datee,purpose,reason) VALUES ('$name','$department','$val','$date','$radio','$reason')")
+or die ("Insert Error:" . mysqli_error($link));
+
+echo"<script>"
+		."alert('Your form has been submitted');"
+		."</script>"
+		."<meta http-equiv=Refresh content=\"0; URL=staffforms.html\">";
+mysqli_close($link);
+?>
