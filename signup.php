79b0dd72cee4ce250ad69ed467c3345f6c3cb24b
Wan Aiman <wanaimannn@gmail.com>
2017-02-08 03:52:07 +0800

    signup staff

--- signup.php
+++ signup.php
@@ -19,8 +19,8 @@ $race = $_POST['race'];
 $religion = $_POST['religion'];
 $maritalstatus = $_POST['maritalstatus'];
 $email = $_POST['email'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST['staff_username'];
+$password = $_POST['staff_pwd'];
 $role = $_POST['role'];
 // foreach($_POST['chkbox'] as $index => $val);
 //$start_date = date('Y-m-d',strtotime($_POST['start_date']));
@@ -28,7 +28,7 @@ $role = $_POST['role'];
 //$reason=mysqli_real_escape_string($link, $_POST['reason']);
 // $verification = $_POST['verification']
 
-$result = mysqli_query ($link,"INSERT INTO biodata (name,ic,address,mobileno,position,department,gender,citizenstatus,race,religion,maritalstatus,email,username,password,role) VALUES ('$name','$ic','$address','$mobileno','$position','$department','$gender','$citizenstatus','$race','$religion','$maritalstatus','$email','$username','$password','$role')")
+$result = mysqli_query ($link,"INSERT INTO staff (name,ic,address,mobileno,position,department,gender,citizenstatus,race,religion,maritalstatus,email,staff_username,staff_pwd,role) VALUES ('$name','$ic','$address','$mobileno','$position','$department','$gender','$citizenstatus','$race','$religion','$maritalstatus','$email','$username','$password','$role')")
 or die ("Insert Error:" . mysqli_error($link));
 
 echo"<script>"
